bbb93d75de84007674d165ba213ddcc5ca914868
boeschj <boeschj@users.noreply.github.com>
2023-05-21 15:41:08 -0500

    Rename Project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# blocktrace
+# ChainLens
 
-Blocktrace is a visual blockchain explorer built with the react-flow and d3.js graphing libraries. The purpose of this project is to make it easier to follow a transaction across the blockchain, providing more transparency on the acitivity of an address.
+ChainLens is a visual blockchain explorer built with the react-flow and d3.js graphing libraries. The purpose of this project is to make it easier to follow a transaction across the blockchain, providing more transparency on the acitivity of an address.
 
-Blocktrace currently supports searching for any address or smart contract on Ethereum, Polygon, Binance Smart Chain, Bitcoin, Litecoin, Zcash, and Dogecoin.
+ChainLens currently supports searching for any address or smart contract on Ethereum, Polygon, Binance Smart Chain, Bitcoin, Litecoin, Zcash, and Dogecoin.
--- client/src/pages/TransactionFlow.tsx
+++ client/src/pages/TransactionFlow.tsx
@@ -1,77 +1,88 @@
-import React, { useState } from 'react';
-import blocktraceLogo from '../assets/blocktraceLogo.png';
-import {
-  ReactFlowProvider,
-} from 'react-flow-renderer';
-import { Button, Input, Modal, Row } from 'antd';
-import moment from 'moment';
-import TransactionFlowGraph from '../components/TransactionFlowGraph';
-import DateRangePicker from '../components/DateRangePicker';
-import DropdownListSelect from '../components/DropdownListSelect';
-import { BitqueryNetworksEnum } from '../constants/BitqueryNetworksEnum';
+import { Button, Input, Modal, Row } from "antd";
+import moment from "moment";
+import React, { useState } from "react";
+import { ReactFlowProvider } from "react-flow-renderer";
+import DateRangePicker from "../components/DateRangePicker";
+import DropdownListSelect from "../components/DropdownListSelect";
+import TransactionFlowGraph from "../components/TransactionFlowGraph";
+import { BitqueryNetworksEnum } from "../constants/BitqueryNetworksEnum";
 
 export interface IQueryParams {
-  inboundDepth: number,
-  outboundDepth: number,
-  network: string,
-  address: string,
-  currency: string,
-  from: string,
-  till: string,
+  inboundDepth: number;
+  outboundDepth: number;
+  network: string;
+  address: string;
+  currency: string;
+  from: string;
+  till: string;
 }
 
 const TransactionFlow: React.FC = (): JSX.Element => {
-  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState("");
   const [queryParams, setQueryParams] = useState<IQueryParams>({
     inboundDepth: 1,
     outboundDepth: 1,
     network: BitqueryNetworksEnum.EthMainnet,
-    address: '',
-    currency: 'ETH',
-    from: moment().startOf('week').toISOString(),
-    till: moment().toISOString()
+    address: "",
+    currency: "ETH",
+    from: moment().startOf("week").toISOString(),
+    till: moment().toISOString(),
   });
 
   const [search, setSearch] = useState(false);
 
   const validateAddressAndSearch = () => {
-    address && address !== '' ? setSearch(true) : Modal.error(
-      {
-        title: 'Error: No Address Entered',
-        content: 'No address was entered, please enter a valid address and try again.'
-      });
+    address && address !== ""
+      ? setSearch(true)
+      : Modal.error({
+          title: "Error: No Address Entered",
+          content:
+            "No address was entered, please enter a valid address and try again.",
+        });
   };
 
   return (
     <div className="h-screen">
-      <div className="flex flex-row content-center space-x-5 justify-start align-center h-[100px] bg-zinc-900">
-        <img src={blocktraceLogo} alt="" height={'100px'} width={'100px'} />
+      <div className="flex flex-row content-center space-x-5 justify-start align-center h-[100px] bg-blue-500">
+        {/* <img src={chainLensLogo} alt="" height={'100px'} width={'100px'} /> */}
         <div className="text-[50px] text-white self-center font-mono">
-          BlockTrace
+          ChainLens
         </div>
       </div>
       <div className="h-20 flex items-center justify-center px-2">
         <Row className="w-full flex items-center space-x-2">
-          <DropdownListSelect queryParams={queryParams} setQueryParams={setQueryParams} />
-          <DateRangePicker queryParams={queryParams} setQueryParams={setQueryParams} />
+          <DropdownListSelect
+            queryParams={queryParams}
+            setQueryParams={setQueryParams}
+          />
+          <DateRangePicker
+            queryParams={queryParams}
+            setQueryParams={setQueryParams}
+          />
           <Input
             onChange={(e) => {
               setAddress(e.target.value.trim());
               setQueryParams({ ...queryParams, address: e.target.value });
             }}
-            style={{ width: '500px' }}
+            style={{ width: "500px" }}
             onPressEnter={validateAddressAndSearch}
             placeholder="Search for any wallet or smart contract address"
           />
           <Button
             style={{ backgroundColor: "#18181b", color: "#ffffff" }}
-            onClick={validateAddressAndSearch}>
+            onClick={validateAddressAndSearch}
+          >
             Search
           </Button>
         </Row>
       </div>
       <ReactFlowProvider>
-        <TransactionFlowGraph address={address} search={search} setSearch={setSearch} queryParams={queryParams} />
+        <TransactionFlowGraph
+          address={address}
+          search={search}
+          setSearch={setSearch}
+          queryParams={queryParams}
+        />
       </ReactFlowProvider>
     </div>
   );
